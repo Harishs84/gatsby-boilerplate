ecb05580abe7d62eb5b71ed0310acb69a9ae16b0
Harish <harish.charan@gmail.com>
2020-12-17 12:11:38 +0530

    Add files via upload

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+import Head from '../components/head'
+
+const IndexPage = () => {
+    return (
+        <Layout>
+            <Head title="Home" />
+            <h2>FUll Stack development using Gatsby - React, GraphQL, Nodejs & Contentful</h2>
+        </Layout>
+    )
+}
+
+export default IndexPage
